4d9314f7e424576082b6dd2ce12eb670e61fcb98
Ryan Domingue <rychdom+github@gmail.com>
2019-04-25 21:28:48 -0700

    Adds bubbling to events

--- utils/events.js
+++ utils/events.js
@@ -3,18 +3,78 @@
  * @type {Object}
  */
 const events = {
-  click: (new MouseEvent('click', {})),
-  keydownTab: (new KeyboardEvent('keydown', { keyCode: 9 })),
-  keydownShiftTab: (new KeyboardEvent('keydown', { keyCode: 9, shiftKey: true })), // eslint-disable-line max-len
-  keydownReturn: (new KeyboardEvent('keydown', { keyCode: 13 })),
-  keydownEsc: (new KeyboardEvent('keydown', { keyCode: 27 })),
-  keydownSpace: (new KeyboardEvent('keydown', { keyCode: 32 })),
-  keydownEnd: (new KeyboardEvent('keydown', { keyCode: 35 })),
-  keydownHome: (new KeyboardEvent('keydown', { keyCode: 36 })),
-  keydownLeft: (new KeyboardEvent('keydown', { keyCode: 37 })),
-  keydownUp: (new KeyboardEvent('keydown', { keyCode: 38 })),
-  keydownRight: (new KeyboardEvent('keydown', { keyCode: 39 })),
-  keydownDown: (new KeyboardEvent('keydown', { keyCode: 40 })),
+  click: (
+    new MouseEvent(
+      'click',
+      { bubbles: true }
+    )
+  ),
+  keydownTab: (
+    new KeyboardEvent(
+      'keydown',
+      { keyCode: 9, bubbles: true }
+    )
+  ),
+  keydownShiftTab: (
+    new KeyboardEvent(
+      'keydown',
+      { keyCode: 9, shiftKey: true, bubbles: true }
+    )
+  ),
+  keydownReturn: (
+    new KeyboardEvent(
+      'keydown',
+      { keyCode: 13, bubbles: true }
+    )
+  ),
+  keydownEsc: (
+    new KeyboardEvent(
+      'keydown',
+      { keyCode: 27, bubbles: true }
+    )
+  ),
+  keydownSpace: (
+    new KeyboardEvent(
+      'keydown',
+      { keyCode: 32, bubbles: true }
+    )
+  ),
+  keydownEnd: (
+    new KeyboardEvent(
+      'keydown',
+      { keyCode: 35, bubbles: true }
+    )
+  ),
+  keydownHome: (
+    new KeyboardEvent(
+      'keydown',
+      { keyCode: 36, bubbles: true }
+    )
+  ),
+  keydownLeft: (
+    new KeyboardEvent(
+      'keydown',
+      { keyCode: 37, bubbles: true }
+    )
+  ),
+  keydownUp: (
+    new KeyboardEvent(
+      'keydown',
+      { keyCode: 38, bubbles: true }
+    )
+  ),
+  keydownRight: (
+    new KeyboardEvent(
+      'keydown',
+      { keyCode: 39, bubbles: true }
+    )
+  ),
+  keydownDown: (
+    new KeyboardEvent(
+      'keydown',
+      { keyCode: 40, bubbles: true }
+    )
+  ),
 };
 
 export default events;
