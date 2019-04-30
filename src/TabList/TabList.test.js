7366ab0f299241016c7a8593654497b80a8819dc
Ryan Domingue <rychdom+github@gmail.com>
2019-04-29 22:00:03 -0700

    Adds the TabList class and related tests

---
+++ src/TabList/TabList.test.js
@@ -0,0 +1,230 @@
+/* eslint-disable max-len */
+import TabList from '.';
+import events from '../../utils/events';
+
+const {
+  click,
+  keydownTab,
+  keydownShiftTab,
+  keydownLeft,
+  keydownRight,
+  keydownDown,
+} = events;
+
+// Set up our document body
+document.body.innerHTML = `
+  <ul class="tablist">
+    <li><a href="#first-panel"></a></li>
+    <li><a href="#second-panel"></a></li>
+    <li><a href="#third-panel"></a></li>
+  </ul>
+  <div id="first-panel" class="panel">
+    <h1>The Article Title</h1>
+    <p>Lorem ipsum dolor sit amet, <a href="example.com/first">consectetur</a>
+    adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+    officia deserunt mollit anim id est laborum.</p>
+  </div>
+  <div id="second-panel" class="panel">
+    <h1>The Article Title</h1>
+    <p>Lorem ipsum dolor sit amet, <a href="example.com/second">consectetur</a>
+    adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+    officia deserunt mollit anim id est laborum.</p>
+  </div>
+  <div id="third-panel" class="panel">
+    <h1>The Article Title</h1>
+    <p>Lorem ipsum dolor sit amet, <a href="example.com/third">consectetur</a>
+    adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+    officia deserunt mollit anim id est laborum.</p>
+  </div>
+`;
+
+const tabs = document.querySelector('.tablist');
+const panels = document.querySelectorAll('.panel');
+
+let tablist = {};
+
+// Cached selectors.
+const firstTab = document.querySelector('a[href="#first-panel"]');
+const secondTab = document.querySelector('a[href="#second-panel"]');
+const thirdTab = document.querySelector('a[href="#third-panel"]');
+const firstPanel = document.querySelector('#first-panel');
+const secondPanel = document.querySelector('#second-panel');
+const thirdPanel = document.querySelector('#third-panel');
+
+describe('TabList with default configuration', () => {
+  beforeEach(() => {
+    tablist = new TabList({ tablist: tabs, panels });
+  });
+
+  describe('TabList adds and manipulates DOM element attributes', () => {
+    it('Should be instantiated as expected', () => {
+      expect(tablist).toBeInstanceOf(TabList);
+
+      expect(tablist.getState().activeIndex).toEqual(0);
+    });
+
+    it('Should add the correct attributes and overlay element',
+      () => {
+        expect(tabs.getAttribute('role')).toEqual('tablist');
+
+        // List items
+        Array.from(tabs.children).forEach((child) => {
+          expect(child.getAttribute('role')).toEqual('presentation');
+        });
+
+        Array.from(tabs.querySelectorAll('a[href]')).forEach((tab, index) => {
+          expect(tab.getAttribute('role')).toEqual('tab');
+          expect(tab.getAttribute('aria-selected')).toEqual((0 === index) ? 'true' : null);
+          expect(tab.getAttribute('tabindex')).toEqual((0 === index) ? null : '-1');
+          expect(tab.id).not.toBeNull();
+        });
+
+        Array.from(panels).forEach((panel, index) => {
+          expect(panel.getAttribute('role')).toEqual('tabpanel');
+          expect(panel.getAttribute('aria-hidden')).toEqual(`${0 !== index}`);
+          expect(panel.getAttribute('aria-labelledby')).not.toBeNull();
+          expect(panel.id).not.toBeNull();
+        });
+      });
+  });
+
+  describe('TabList methods work as expected', () => {
+    it('Should switch to the specified tab', () => {
+      tablist.switchTo(0);
+      expect(firstTab.getAttribute('aria-selected')).toEqual('true');
+      expect(secondTab.getAttribute('aria-selected')).toBeNull();
+      expect(thirdTab.getAttribute('aria-selected')).toBeNull();
+
+      expect(firstTab.getAttribute('tabindex')).toBeNull();
+      expect(secondTab.getAttribute('tabindex')).toEqual('-1');
+      expect(thirdTab.getAttribute('tabindex')).toEqual('-1');
+
+      expect(firstPanel.getAttribute('aria-hidden')).toEqual('false');
+      expect(secondPanel.getAttribute('aria-hidden')).toEqual('true');
+      expect(thirdPanel.getAttribute('aria-hidden')).toEqual('true');
+
+      tablist.switchTo(2);
+      expect(firstTab.getAttribute('aria-selected')).toBeNull();
+      expect(secondTab.getAttribute('aria-selected')).toBeNull();
+      expect(thirdTab.getAttribute('aria-selected')).toEqual('true');
+
+      expect(firstTab.getAttribute('tabindex')).toEqual('-1');
+      expect(secondTab.getAttribute('tabindex')).toEqual('-1');
+      expect(thirdTab.getAttribute('tabindex')).toBeNull();
+
+      expect(firstPanel.getAttribute('aria-hidden')).toEqual('true');
+      expect(secondPanel.getAttribute('aria-hidden')).toEqual('true');
+      expect(thirdPanel.getAttribute('aria-hidden')).toEqual('false');
+    });
+
+    it('Should remove all DOM attributes when destroyed', () => {
+      tablist.destroy();
+      expect(tabs.getAttribute('role')).toBeNull();
+
+      // List items
+      Array.from(tabs.children).forEach((child) => {
+        expect(child.getAttribute('role')).toBeNull();
+      });
+
+      Array.from(tabs.querySelectorAll('a[href]')).forEach((tab) => {
+        expect(tab.getAttribute('role')).toBeNull();
+        expect(tab.getAttribute('aria-selected')).toBeNull();
+        expect(tab.getAttribute('tabindex')).toBeNull();
+      });
+
+      Array.from(panels).forEach((panel) => {
+        expect(panel.getAttribute('role')).toBeNull();
+        expect(panel.getAttribute('aria-hidden')).toBeNull();
+
+        const firstChild = panel.querySelector('a[href]');
+        expect(firstChild.getAttribute('tabindex')).toBeNull();
+      });
+    });
+  });
+
+  describe('TabList correctly responds to events', () => {
+    it('Should update attributes when the tabs are clicked', () => {
+      firstTab.dispatchEvent(click);
+      expect(firstTab.getAttribute('aria-selected')).toEqual('true');
+      expect(secondTab.getAttribute('aria-selected')).toBeNull();
+      expect(thirdTab.getAttribute('aria-selected')).toBeNull();
+
+      expect(firstTab.getAttribute('tabindex')).toBeNull();
+      expect(secondTab.getAttribute('tabindex')).toEqual('-1');
+      expect(thirdTab.getAttribute('tabindex')).toEqual('-1');
+
+      expect(firstPanel.getAttribute('aria-hidden')).toEqual('false');
+      expect(secondPanel.getAttribute('aria-hidden')).toEqual('true');
+      expect(thirdPanel.getAttribute('aria-hidden')).toEqual('true');
+
+      thirdTab.dispatchEvent(click);
+      expect(firstTab.getAttribute('aria-selected')).toBeNull();
+      expect(secondTab.getAttribute('aria-selected')).toBeNull();
+      expect(thirdTab.getAttribute('aria-selected')).toEqual('true');
+
+      expect(firstTab.getAttribute('tabindex')).toEqual('-1');
+      expect(secondTab.getAttribute('tabindex')).toEqual('-1');
+      expect(thirdTab.getAttribute('tabindex')).toBeNull();
+
+      expect(firstPanel.getAttribute('aria-hidden')).toEqual('true');
+      expect(secondPanel.getAttribute('aria-hidden')).toEqual('true');
+      expect(thirdPanel.getAttribute('aria-hidden')).toEqual('false');
+    });
+
+    it('Should handle keyboard evets to and from tabs', () => {
+      firstTab.dispatchEvent(click);
+      const firstPanelChild = firstPanel.querySelector('a[href]');
+
+      firstTab.dispatchEvent(keydownTab);
+      expect(document.activeElement).toEqual(firstPanelChild);
+
+      firstPanelChild.dispatchEvent(keydownShiftTab);
+      expect(document.activeElement).toEqual(firstTab);
+    });
+
+    it('Should switch tabs when arrow keys are pressed', () => {
+      firstTab.focus();
+      firstTab.dispatchEvent(keydownRight);
+      expect(document.activeElement).toEqual(secondTab);
+      expect(secondTab.getAttribute('aria-selected')).toEqual('true');
+
+      secondTab.dispatchEvent(keydownRight);
+      expect(document.activeElement).toEqual(thirdTab);
+      expect(thirdTab.getAttribute('aria-selected')).toEqual('true');
+
+      thirdTab.dispatchEvent(keydownRight);
+      expect(document.activeElement).toEqual(thirdTab); // don't cycle
+      expect(thirdTab.getAttribute('aria-selected')).toEqual('true');
+
+      thirdTab.dispatchEvent(keydownLeft);
+      expect(document.activeElement).toEqual(secondTab);
+      expect(secondTab.getAttribute('aria-selected')).toEqual('true');
+
+      secondTab.dispatchEvent(keydownLeft);
+      expect(document.activeElement).toEqual(firstTab);
+      expect(firstTab.getAttribute('aria-selected')).toEqual('true');
+
+      firstTab.dispatchEvent(keydownLeft);
+      expect(document.activeElement).toEqual(firstTab); // don't cycle
+      expect(firstTab.getAttribute('aria-selected')).toEqual('true');
+    });
+
+    it('Should focus the panel from tab when down arrow pressed', () => {
+      secondTab.focus();
+      secondTab.dispatchEvent(keydownDown);
+      expect(document.activeElement).toEqual(secondPanel);
+    });
+  });
+});
---
+++ src/TabList/index.js
@@ -0,0 +1,275 @@
+import AriaComponent from '../AriaComponent';
+import interactiveChildren from '../lib/interactiveChildren';
+import { setUniqueId } from '../lib/uniqueId';
+import rovingTabIndex from '../lib/rovingTabIndex';
+import keyCodes from '../lib/keyCodes';
+
+/**
+ * Manage tablist elements
+ */
+export default class Tablist extends AriaComponent {
+  constructor(config) {
+    super();
+
+    if ('UL' !== config.tablist.nodeName) {
+      // @todo Maybe throw an error here?
+      return;
+    }
+
+    /**
+     * The component name.
+     * @type {String}
+     */
+    this.componentName = 'dialog';
+
+    /**
+     * Default config options.
+     * @type {Object}
+     */
+    const options = {
+      tablist: null,
+      panels: null,
+    };
+
+    // Merge config options with defaults.
+    Object.assign(this, options, config);
+
+    // Default state.
+    this.state = {
+      activeIndex: 0,
+    };
+
+    // Bind class methods
+    this.handleTabKeydown = this.handleTabKeydown.bind(this);
+    this.handleTabsKeydown = this.handleTabsKeydown.bind(this);
+    this.handleTabsClick = this.handleTabsClick.bind(this);
+    this.switchTo = this.switchTo.bind(this);
+    this.destroy = this.destroy.bind(this);
+
+    /**
+     * Tablist panels
+     * @type {Array}
+     */
+    this.panels = Array.prototype.slice.call(this.panels);
+
+    /**
+     * Collect the anchor inside of each list item.
+     * Required markup is `<li><a href=""></a></li>`
+     * @type {Array}
+     */
+    this.tabs = Array.prototype.filter.call(
+      this.tablist.children,
+      (child) => null !== child.querySelector('a[href]')
+    )
+      .map((child) => child.querySelector('a[href]'));
+
+    // Only initialize if tabs aren panels are equal in number.
+    if (this.tabs.length === this.panels.length) {
+      this.init();
+    }
+  }
+
+  /**
+   * Update tab and panel attributes based on new state.
+   */
+  stateWasUpdated({ activeIndex }) {
+    const [deactivate] = this.tabs.filter((tab) => (
+      'true' === tab.getAttribute('aria-selected')
+    ));
+    const deactiveIndex = this.tabs.indexOf(deactivate);
+
+    // Deactivate the previously-selected tab.
+    deactivate.setAttribute('tabindex', '-1');
+    deactivate.removeAttribute('aria-selected');
+    this.panels[deactiveIndex].setAttribute('aria-hidden', 'true');
+
+    const deactiveChildren = interactiveChildren(this.panels[deactiveIndex]);
+    // Prevent tabbing to interactive children of the deactivated panel.
+    rovingTabIndex(deactiveChildren);
+
+    // Actvate the newly active tab.
+    this.tabs[activeIndex].removeAttribute('tabindex');
+    this.tabs[activeIndex].setAttribute('aria-selected', 'true');
+    this.panels[activeIndex].setAttribute('aria-hidden', 'false');
+
+    // Allow tabbing to the newly-active panel.
+    this.interactiveChildren = interactiveChildren(this.panels[activeIndex]);
+    rovingTabIndex(this.interactiveChildren, this.interactiveChildren);
+  }
+
+  /**
+   * Add necessary attributes and event listeners; collect interactive elements.
+   */
+  init() {
+    const { activeIndex } = this.state;
+
+    // The list gets the `tablist` role.
+    this.tablist.setAttribute('role', 'tablist');
+
+    // Each of the list items are now presentational.
+    Array.prototype.forEach.call(this.tablist.children, (listChild) => {
+      if ('LI' === listChild.nodeName) {
+        listChild.setAttribute('role', 'presentation');
+      }
+    });
+
+    this.tabs.forEach((tab, index) => {
+      // Ensure each tab has an ID.
+      setUniqueId(tab);
+      // Add the `tab` role.
+      tab.setAttribute('role', 'tab');
+
+      if (activeIndex !== index) {
+        // Don't allow focus on inactive tabs.
+        tab.setAttribute('tabindex', '-1');
+      } else {
+        // Set up the selected tab.
+        tab.setAttribute('aria-selected', 'true');
+      }
+    });
+
+    this.tablist.addEventListener('click', this.handleTabsClick);
+    this.tablist.addEventListener('keydown', this.handleTabsKeydown);
+
+    this.panels.forEach((panel, index) => {
+      // Ensure each panel has an ID.
+      setUniqueId(panel);
+      // Add the `tabpanel` role.
+      panel.setAttribute('role', 'tabpanel');
+      // Create a relationship between the tab and its panel.
+      panel.setAttribute('aria-labelledby', this.tabs[index].id);
+      // All but the first tab should be hidden by default.
+      panel.setAttribute('aria-hidden', `${activeIndex !== index}`);
+      // Listen for panel keydown events.
+      panel.addEventListener('keydown', this.handleTabKeydown);
+    });
+
+    // Save the active panel's interactive children.
+    this.interactiveChildren = interactiveChildren(this.panels[activeIndex]);
+  }
+
+  /**
+   * Handle keydown events on the tabpanels.
+   *
+   * @param {Object} event The event object.
+   */
+  handleTabKeydown(event) {
+    const { TAB } = keyCodes;
+    const { keyCode, shiftKey } = event;
+
+    // Shift-TAB from the active panel's first interactive element.
+    if (keyCode === TAB && shiftKey) {
+      // eslint-disable-next-line max-len
+      const focusIndex = this.interactiveChildren.indexOf(document.activeElement);
+      const [theTab] = this.tabs.filter(
+        (tab) => tab.hasAttribute('aria-selected')
+      );
+
+      // This is the first interactive child element and there is an active tab.
+      if (0 === focusIndex && null !== theTab) {
+        event.preventDefault();
+        theTab.focus();
+      }
+    }
+  }
+
+  /**
+   * Handle tablist key presses.
+   *
+   * @param {Object} event The event object.
+   */
+  handleTabsKeydown(event) {
+    const {
+      TAB,
+      LEFT,
+      RIGHT,
+      DOWN,
+    } = keyCodes;
+    const { keyCode, shiftKey, target } = event;
+    const currentIndex = this.tabs.indexOf(target);
+
+    if (keyCode === TAB && ! shiftKey && this.interactiveChildren.length) {
+      event.preventDefault();
+      // Move focus from the active tab to the active panel's first child.
+      this.interactiveChildren[0].focus();
+    } else if ([LEFT, RIGHT, DOWN].includes(keyCode)) {
+      // Navigate through tablist with arrow keys.
+      if ([LEFT, RIGHT].includes(keyCode)) {
+        const newIndex = (LEFT === keyCode)
+          ? currentIndex - 1
+          : currentIndex + 1;
+
+        if (undefined !== this.tabs[newIndex]) {
+          event.preventDefault();
+          this.switchTo(newIndex);
+          this.tabs[newIndex].focus();
+        }
+      } else if (DOWN === keyCode) {
+        // Focus the active panel itself with the down arrow.
+        event.preventDefault();
+        this.panels[currentIndex].setAttribute('tabindex', '-1');
+        this.panels[currentIndex].focus();
+      }
+    }
+  }
+
+  /**
+   * Toggle tabs/panels.
+   *
+   * @param {Object} event The event object.
+   */
+  handleTabsClick(event) {
+    const { target } = event;
+    event.preventDefault();
+
+    // Don't act when an active tab is clicked.
+    if ('true' !== target.getAttribute('aria-selected')) {
+      const index = this.tabs.indexOf(target);
+      this.switchTo(index);
+    }
+  }
+
+  /**
+   * Switch directly to a tab.
+   *
+   * @param  {Number} index The zero-based tab index to activate.
+   */
+  switchTo(index) {
+    this.setState({ activeIndex: index });
+  }
+
+  /**
+   * Destroy the tablist, removing ARIA attributes and event listeners
+   */
+  destroy() {
+    this.tablist.removeAttribute('role');
+
+    Array.prototype.forEach.call(this.tablist.children, (listChild) => {
+      if ('LI' === listChild.nodeName) {
+        listChild.removeAttribute('role');
+      }
+    });
+
+    this.tabs.forEach((tab) => {
+      tab.removeAttribute('role');
+      tab.removeAttribute('aria-selected');
+      tab.removeAttribute('tabindex');
+
+      tab.removeEventListener('click', this.handleTabsClick);
+      tab.removeEventListener('keydown', this.handleTabsKeydown);
+    });
+
+    this.panels.forEach((panel) => {
+      panel.removeAttribute('role');
+      panel.removeAttribute('aria-hidden');
+
+      const interactiveChildElements = interactiveChildren(panel);
+      rovingTabIndex(interactiveChildElements, interactiveChildElements);
+
+      panel.removeEventListener(
+        'keydown',
+        this.handleTabKeydown
+      );
+    });
+  }
+}
