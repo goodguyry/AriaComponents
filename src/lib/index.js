09916db83c7ed4b7c252a0107ecaf5336a16bb2a
Ryan Domingue <rychdom+github@gmail.com>
2019-05-03 18:42:54 -0700

    Exports lib files

---
+++ src/lib/index.js
@@ -0,0 +1,15 @@
+import createScreenReaderText from './createScreenReaderText';
+import instanceOf from './instanceOf';
+import interactiveChildren from './interactiveChildren';
+import keyCodes from './keyCodes';
+import rovingTabIndex from './rovingTabIndex';
+import uniqueId from './uniqueId';
+
+export {
+  createScreenReaderText,
+  instanceOf,
+  interactiveChildren,
+  keyCodes,
+  rovingTabIndex,
+  uniqueId,
+};
