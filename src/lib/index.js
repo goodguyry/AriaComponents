b35cb378d18746644f03e5d3f8eba03b7f5b08ef
Ryan Domingue <rychdom+github@gmail.com>
2019-10-13 09:17:30 -0700

    Adds missing lib exports

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,15 +1,19 @@
 import ariaDescribedbyElementsFound from './ariaDescribedbyElementsFound';
-import isInstanceOf from './isInstanceOf';
 import interactiveChildren from './interactiveChildren';
+import isInstanceOf from './isInstanceOf';
 import keyCodes from './keyCodes';
+import nextPrevious from './nextPrevious';
 import rovingTabIndex from './rovingTabIndex';
+import typeAhead from './typeAhead';
 import uniqueId from './uniqueId';
 
 export {
   ariaDescribedbyElementsFound,
-  isInstanceOf,
   interactiveChildren,
+  isInstanceOf,
   keyCodes,
+  nextPrevious,
   rovingTabIndex,
+  typeAhead,
   uniqueId,
 };
