ba0201e29d16a07623a6d42c2f174bbe04e79eff
Ryan Domingue <rychdom+github@gmail.com>
2023-01-02 17:27:07 -0800

    Adds todos

--- src/Tablist/index.js
+++ src/Tablist/index.js
@@ -189,6 +189,7 @@ export default class Tablist extends AriaComponent {
     const { activeIndex } = this.state;
 
     // Get the tab currently designated as `aria-selected`.
+    // @todo Switch to `find()`.
     const [deactivate] = this.tabLinks.filter((tab) => (
       'true' === tab.getAttribute('aria-selected')
     ));
@@ -353,6 +354,7 @@ export default class Tablist extends AriaComponent {
     event.preventDefault();
 
     // Don't act when an active tab is clicked.
+    // @todo Simplify this into one condition.
     if ('true' !== target.getAttribute('aria-selected')) {
       if (this.tabLinks.includes(target)) {
         this.switchTo(this.tabLinks.indexOf(target));
