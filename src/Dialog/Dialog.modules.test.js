c31b4333187a5bf00e9d1ec05218b01fae37e915
Ryan Domingue <rychdom+github@gmail.com>
2023-01-26 22:22:44 -0800

    Merges module tests by component

---
+++ src/Dialog/Dialog.modules.test.js
@@ -0,0 +1,124 @@
+/* eslint-disable max-len */
+import Dialog from '.';
+import {
+  ManageTabIndex,
+  UseButtonRole,
+  UseHiddenAttribute,
+} from './modules';
+import { events } from '../../.jest/events';
+
+const { keydownEnter, keydownSpace } = events;
+
+// Set up our document body
+document.body.innerHTML = `
+  <main>
+    <article>
+      <h1>The Article Title</h1>
+      <a href="#" class="outside-link">Link</a>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+      ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+      sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum.</p>
+      <a aria-controls="dialog" class="link" href="#dialog">Open dialog</a>
+    </article>
+  </main>
+  <footer class="site-footer">Site footer</footer>
+  <div class="wrapper" id="dialog" tabindex="0">
+    <button>Close</button>
+    <ul>
+      <li><a href="example.com"></a></li>
+      <li><a href="example.com"></a></li>
+      <li><a href="example.com"></a></li>
+      <li><a class="last-item" href="example.com"></a></li>
+    </ul>
+  </div>
+`;
+
+const controller = document.querySelector('[aria-controls="dialog"]');
+const target = document.getElementById('dialog');
+
+let dialog;
+beforeEach(() => {
+  dialog = new Dialog(
+    controller,
+    {
+      modules: [
+        ManageTabIndex,
+        UseButtonRole,
+        UseHiddenAttribute,
+      ],
+    }
+  );
+});
+
+test('ManageTabIndex: Manage target element tabindex', () => {
+  dialog.interactiveChildElements.forEach((link) => {
+    expect(link.getAttribute('tabindex')).toEqual('-1');
+  });
+
+  dialog.expanded = true;
+  expect(dialog.expanded).toBe(true);
+  dialog.interactiveChildElements.forEach((link) => {
+    expect(link.getAttribute('tabindex')).toBeNull();
+  });
+
+  dialog.expanded = false;
+  expect(dialog.expanded).toBe(false);
+  dialog.interactiveChildElements.forEach((link) => {
+    expect(link.getAttribute('tabindex')).toEqual('-1');
+  });
+
+  dialog.destroy();
+  dialog.interactiveChildElements.forEach((link) => {
+    expect(link.getAttribute('tabindex')).toBeNull();
+  });
+});
+
+describe('UseButtonRole', () => {
+  beforeEach(() => {
+    dialog.expanded = false;
+  });
+
+  test('Should use the button role on the controller', () => {
+    expect(controller.getAttribute('role')).toBe('button');
+    expect(controller.getAttribute('tabindex')).not.toBe('0');
+  });
+
+  test('The Return key and Spacebar activate the Dialog target', () => {
+    // Ensure the Dialog is closed.
+    expect(dialog.expanded).toBe(false);
+
+    // Enter.
+    controller.dispatchEvent(keydownEnter);
+    expect(dialog.expanded).toBe(true);
+
+    // Spacebar.
+    controller.dispatchEvent(keydownSpace);
+    expect(dialog.expanded).toBe(true);
+  });
+
+  test.skip('Module cleanup runs', () => {
+    dialog.destroy();
+    expect(controller.getAttribute('role')).toBeNull();
+    expect(controller.getAttribute('tabindex')).toBeNull();
+  });
+});
+
+test('UseHiddenAttribute: Uses hidden attribute when target not expanded', () => {
+  expect(dialog.expanded).toBe(false);
+  expect(target.getAttribute('hidden')).toBe('');
+
+  dialog.expanded = true;
+  expect(dialog.expanded).toBe(true);
+  expect(target.getAttribute('hidden')).toBeNull();
+
+  dialog.expanded = false;
+  expect(dialog.expanded).toBe(false);
+  expect(target.getAttribute('hidden')).toBe('');
+
+  dialog.destroy();
+  expect(target.getAttribute('hidden')).toBeNull();
+});
---
+++ src/Popup/Popup.modules.test.js
@@ -0,0 +1,127 @@
+/* eslint-disable max-len */
+import Popup from '.';
+import {
+  ComponentConnector,
+  ManageTabIndex,
+  UseButtonRole,
+  UseHiddenAttribute,
+} from './modules';
+import { events } from '../../.jest/events';
+
+const {
+  keydownEnter,
+  keydownShiftTab,
+  keydownSpace,
+  keydownTab,
+} = events;
+
+// Set up our document body
+document.body.innerHTML = `
+  <a aria-controls="dropdown" href="#dropdown" class="link">Open</a>
+  <span>Break up DOM</span>
+  <div class="wrapper" id="dropdown">
+    <ul>
+      <li><a class="first-child" href="example.com"></a></li>
+      <li><a href="example.com"></a></li>
+      <li><a href="example.com"></a></li>
+      <li><a class="last-child" href="example.com"></a></li>
+    </ul>
+  </div>
+`;
+
+const controller = document.querySelector('.link');
+const target = document.querySelector('.wrapper');
+
+let popup;
+beforeEach(() => {
+  popup = new Popup(
+    controller,
+    {
+      modules: [
+        ComponentConnector,
+        ManageTabIndex,
+        UseButtonRole,
+        UseHiddenAttribute,
+      ],
+    }
+  );
+
+  popup.init();
+});
+
+const targetFirstChild = document.querySelector('.first-child');
+
+test('ComponentConnector: Connect disconnected controller-target pair', () => {
+  expect(controller.getAttribute('aria-owns')).toEqual(target.id);
+
+  popup.expanded = true;
+  expect(popup.expanded).toBe(true);
+
+  controller.focus();
+  controller.dispatchEvent(keydownTab);
+  expect(document.activeElement).toEqual(targetFirstChild);
+
+  target.dispatchEvent(keydownShiftTab);
+  expect(document.activeElement).toEqual(controller);
+
+  popup.destroy();
+  expect(controller.getAttribute('aria-owns')).toBeNull();
+});
+
+test('ManageTabIndex: Manage target element tabindex', () => {
+  // Initial state.
+  popup.interactiveChildElements.forEach((link) => expect(link.getAttribute('tabindex')).toEqual('-1'));
+
+  popup.expanded = true;
+  expect(popup.expanded).toBe(true);
+  popup.interactiveChildElements.forEach((link) => expect(link.getAttribute('tabindex')).toBeNull());
+
+  popup.expanded = false;
+  expect(popup.expanded).toBe(false);
+  popup.interactiveChildElements.forEach((link) => expect(link.getAttribute('tabindex')).toEqual('-1'));
+
+  popup.destroy();
+  popup.interactiveChildElements.forEach((link) => expect(link.getAttribute('tabindex')).toBeNull());
+});
+
+describe('UseButtonRole', () => {
+  test('Should use the button role on the controller', () => {
+    expect(controller.getAttribute('role')).toBe('button');
+    expect(controller.getAttribute('tabindex')).not.toBe('0');
+  });
+
+  test('The Return key and Spacebar activate the Popup target', () => {
+    // Ensure the Popup is closed.
+    expect(popup.expanded).toBe(false);
+
+    // Enter.
+    controller.dispatchEvent(keydownEnter);
+    expect(popup.expanded).toBe(true);
+
+    // Spacebar.
+    controller.dispatchEvent(keydownSpace);
+    expect(popup.expanded).toBe(false);
+  });
+
+  test.skip('Module cleanup runs', () => {
+    popup.destroy();
+    expect(controller.getAttribute('role')).toBeNull();
+    expect(controller.getAttribute('tabindex')).toBeNull();
+  });
+});
+
+test('UseHiddenAttribute: Uses hidden attribute when target not expanded', () => {
+  expect(popup.expanded).toBe(false);
+  expect(target.getAttribute('hidden')).toBe('');
+
+  popup.expanded = true;
+  expect(popup.expanded).toBe(true);
+  expect(target.getAttribute('hidden')).toBeNull();
+
+  popup.expanded = false;
+  expect(popup.expanded).toBe(false);
+  expect(target.getAttribute('hidden')).toBe('');
+
+  popup.destroy();
+  expect(target.getAttribute('hidden')).toBeNull();
+});
